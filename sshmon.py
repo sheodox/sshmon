1c996760f175866bb0797f58240bd7f4ffadc4df
sheodox <imagamer27@msn.com>
2019-10-15 22:41:11 -0500

    color code by server online status, easier to see

--- sshmon.py
+++ sshmon.py
@@ -6,7 +6,7 @@ import json
 from pathlib import Path
 
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk, GLib, Gdk
 
 
 class SSHMonWindow(Gtk.Window):
@@ -56,13 +56,17 @@ class SSHMonWindow(Gtk.Window):
 
     def create_server_row(self, server):
         def set_button_text(active):
-            return ssh_button.set_label(f'{server.nickname} - {active} - ({server.hostname})')
+            context = ssh_button.get_style_context()
+            context.remove_class('online')
+            context.remove_class('pinging')
+            context.remove_class('offline')
+            context.add_class(active)
+            return ssh_button.set_label(f'{server.nickname} - ({server.hostname})')
 
         box = self.new_box_row()
-        ssh_button = Gtk.Button(
-            label='', expand=True)
+        ssh_button = Gtk.Button(label='', expand=True)
         delete_button = Gtk.Button(label='Delete')
-        set_button_text('pinging...')
+        set_button_text(server.get_status())
 
         box.add(ssh_button)
         box.add(delete_button)
@@ -116,10 +120,11 @@ class SSHMonWindow(Gtk.Window):
         return self.new_list_child(Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, expand=True, spacing=7))
 
     def show_ssh(self, widget, server):
-        if server.status() == 'online':
+        if server.get_status() == 'online':
             server.ssh()
         else:
-            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK, f'Cannot connect to {server.hostname}')
+            dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.ERROR, Gtk.ButtonsType.OK,
+                                       f'Cannot connect to {server.hostname}')
             dialog.run()
             dialog.destroy()
 
@@ -129,22 +134,21 @@ class Server:
         self.nickname = nickname
         self.username = username
         self.hostname = hostname
-        self.online = None
+        self.status = 'pinging'
 
     def ping(self, callback):
         def in_thread():
             proc = Popen(['/usr/bin/ping', '-c', '1', self.hostname])
             proc.wait()
-            self.online = proc.poll() == 0
-            GLib.idle_add(lambda: callback(self.status()))
+            was_online = proc.poll() == 0
+            self.status = 'online' if was_online else 'offline'
+            GLib.idle_add(lambda: callback(self.get_status()))
 
         thread = threading.Thread(target=in_thread)
         thread.start()
 
-    def status(self):
-        if self.online is None:
-            return 'pinging...'
-        return 'online' if self.online else 'offline'
+    def get_status(self):
+        return self.status
 
     def ssh(self):
         def ssh(cmd):
@@ -157,6 +161,12 @@ class Server:
             ssh(f'konsole -e "ssh {self.username}@{self.hostname}"')
 
 
+css_provider = Gtk.CssProvider()
+css_provider.load_from_path(str(Path(__file__).resolve().parent.joinpath('style.css')))
+screen = Gdk.Screen.get_default()
+styleContext = Gtk.StyleContext()
+styleContext.add_provider_for_screen(screen, css_provider,
+                                     Gtk.STYLE_PROVIDER_PRIORITY_USER)
 win = SSHMonWindow()
 win.connect('destroy', Gtk.main_quit)
 Gtk.main()
