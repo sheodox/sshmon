04333e1bb27a42745cae72a2264c951448011500
sheodox <imagamer27@msn.com>
2023-02-14 15:50:03 -0600

    Use bubble tea to show the selection list
    This is mostly from the examples in the bubbles docs. This allows for a nicer UI and filtering!

--- main.go
+++ main.go
@@ -8,41 +8,15 @@ import (
 	"os"
 	"os/exec"
 	"path"
-	"strconv"
 )
 
 func main() {
 	// disable showing time with log failures
 	log.SetFlags(0)
 
-	config := loadConfig()
+	remotes := loadConfig()
 
-	fmt.Println("Enter the number of a remote you want to connect to:")
-
-	for index, remote := range config {
-		fmt.Printf("%v. %v - %v@%v\n", index+1, remote.Nickname, remote.Username, remote.Hostname)
-	}
-
-	fmt.Print("> ")
-	var choice string
-	_, err := fmt.Scanln(&choice)
-	if err != nil {
-		log.Fatal("Error getting choice", err)
-	}
-
-	choiceNum, err := strconv.Atoi(choice)
-
-	if err != nil {
-		log.Fatal("Error parsing choice", err)
-	}
-
-	choiceIndex := choiceNum - 1
-
-	if choiceIndex < 0 || choiceIndex >= len(config) {
-		log.Fatalf("%v is out of bounds, must be between 1 and %v\n", choiceNum, len(config))
-	}
-	// prompted using position, need to subtract to get index
-	remote := config[choiceIndex]
+	remote := selectRemote(remotes)
 
 	ssh(remote)
 }
---
+++ select.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+var docStyle = lipgloss.NewStyle().Margin(1, 2)
+
+type item struct {
+	title, desc string
+	remote      RemoteConfig
+}
+
+func (i item) Title() string       { return i.title }
+func (i item) Description() string { return i.desc }
+func (i item) FilterValue() string { return i.title }
+
+type model struct {
+	list           list.Model
+	selectedRemote RemoteConfig
+}
+
+func (m *model) Init() tea.Cmd {
+	return nil
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		if msg.String() == "ctrl+c" || msg.String() == "q" {
+			return m, tea.Quit
+		}
+		if msg.String() == "enter" {
+			item, ok := m.list.SelectedItem().(item)
+			if ok {
+				m.selectedRemote = item.remote
+				return m, tea.Quit
+			}
+		}
+	case tea.WindowSizeMsg:
+		h, v := docStyle.GetFrameSize()
+		m.list.SetSize(msg.Width-h, msg.Height-v)
+	}
+
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
+}
+
+func (m *model) View() string {
+	return docStyle.Render(m.list.View())
+}
+
+func selectRemote(remotes []RemoteConfig) RemoteConfig {
+	items := []list.Item{}
+
+	for _, remote := range remotes {
+		items = append(items, item{
+			title:  remote.Nickname,
+			desc:   fmt.Sprintf("%v@%v", remote.Username, remote.Hostname),
+			remote: remote,
+		})
+	}
+
+	m := model{list: list.New(items, list.NewDefaultDelegate(), 0, 0)}
+	m.list.Title = "Select a remote to connect to"
+
+	p := tea.NewProgram(&m, tea.WithAltScreen())
+
+	if _, err := p.Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+
+	return m.selectedRemote
+}
